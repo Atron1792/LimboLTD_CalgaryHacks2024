ef29f010eee0422dcc5d507fa91c9f0773851e44
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2024-02-18 00:35:40 -0700

    eye_enemy

--- enemies/enemy_controller.gd
+++ enemies/enemy_controller.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var placeholder_enemy_scene = preload("res://scenes/mob.tscn")
+var placeholder_enemy_scene = load("res://scenes/mob.tscn")
+var eye_scene = load()
 @onready var timer = 2
 
 # Called when the node enters the scene tree for the first time.
@@ -15,17 +16,23 @@ func _process(delta):
 	#print(timer)
 	if timer < 0:
 		print("spawning")
-		spawn_placeholder(100,100)
+	#	spawn_placeholder(100,100)
 		timer = 2
 	pass
 	
 
 func spawn_placeholder(position_x, position_y):
 	var placeholder_enemy = placeholder_enemy_scene.instantiate()
+	placeholder_enemy.position = Vector2(position_x, position_y)
 	add_child(placeholder_enemy)
 
 
-func _on_timer_timeout():
+func _on_eye_spawner_timeout():
+	var eye = eye_scene.instantiate()
+	pass # Replace with function body.
+
+
+func _on_placeholder_spawn_timeout():
 	print("timer")
 	spawn_placeholder(200,200)
 	pass # Replace with function body.
