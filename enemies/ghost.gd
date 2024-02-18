2789bfdc76b84afd47ea6cb4434b5d8ec0d5cb63
Anoone <72642342+Anooneee@users.noreply.github.com>
2024-02-18 02:18:07 -0700

    idle

--- enemies/ghost.gd
+++ enemies/ghost.gd
@@ -6,13 +6,13 @@ var move_speed:float = 8000.0
 var player
 
 enum {
-	SPAWN,
 	MOVE,
 	DIE
 }
 
 var state = MOVE
 var ghost_health = 2
+var death_timer = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -44,17 +44,19 @@ func move_action(delta):
 	set_movement_target(player.position)
 	velocity = global_position.direction_to(nav_agent.get_next_path_position()) * move_speed * delta
 	move_and_slide()
+	if(ghost_health > 0):
+		ghost_health = ghost_health - 1
 	if (ghost_health <= 0):
 		state = DIE
 
 func die_action(delta):
+	death_timer -= delta
+	if (death_timer <= 0):
 		queue_free()
 
 func get_player():
 	player = get_parent().player
 
 func _on_enemy_hurtbox_area_entered(area):
-	if(ghost_health > 0):
-		ghost_health = ghost_health - 1
 	print("ghost collision")
 	state = MOVE
